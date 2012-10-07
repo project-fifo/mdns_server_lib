a814a19f276e68672c74148c0f74f0ebbe64320f
Heinz N. Gies <heinz@licenser.net>
2012-10-07 21:22:59 +0200

    Added better readme and documentaiton.

--- src/mdns_server_lib.erl
+++ src/mdns_server_lib.erl
@@ -2,6 +2,8 @@
 
 -export([start/0]).
 
+-spec start() -> ok |
+		 {error, Reason::any()}.
 start() ->
     application:start(ranch),
     application:start(mdns_server_lib).
--- src/mdns_server_lib_debug_handler.erl
+++ src/mdns_server_lib_debug_handler.erl
@@ -1,10 +1,40 @@
 -module(mdns_server_lib_debug_handler).
 
-
 -export([init/1, message/2]).
 
+%%--------------------------------------------------------------------
+%% @doc This function is called when the handler is initialized.
+%%  It will return a new state used by the other callbacks.
+%% @spec init(Opts::any()) ->
+%%    {ok, State::any()} |
+%%    {ok, stateless}
+%% @end
+%%--------------------------------------------------------------------
+
+%-sepc init(any()) ->
+%    {ok, State::any()} |
+%    {ok, stateless}.
 init(_Opts) ->
     {ok, stateless}.
+
+%%--------------------------------------------------------------------
+%% @doc message will be called whenever a mesage was received.
+%%  The data can be handed on to your own code and either return a
+%%  value by using `{reply, Reply, NewState}' or just let the call 
+%%  return without a reply by returning `{noreply, NewState}'. If an
+%%  error occurs you can use `{stop, NewState}' which will close the
+%%  connection without a reply at all which.
+%% @spec message(Data::term(), State::term()) ->
+%%		     {reply, Reply::term(), NewState::term()}
+%%			 | {noreply, NewState::term()}
+%%			 | {stop, NewState::term()}
+%% @end
+%%--------------------------------------------------------------------
+
+-spec message(Data::term(), State::term()) ->
+		     {reply, Reply::term(), NewState::term()} |
+		     {noreply, NewState::term()} |
+		     {stop, NewState::term()}.
 
 message(Data, State) ->
     io:format("[zmq] ~p~n", [Data]),
