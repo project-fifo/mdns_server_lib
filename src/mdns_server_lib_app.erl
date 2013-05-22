5d49f8966293c769f0387ef5af5165c1e69fdf8c
Heinz N. Gies <heinz@licenser.net>
2013-05-22 16:56:48 +0200

    Added interface selection.

--- src/mdns_server_lib_app.erl
+++ src/mdns_server_lib_app.erl
@@ -14,13 +14,19 @@ start(_StartType, _StartArgs) ->
     {ok, Service} = application:get_env(mdns_server_lib, service),
     {ok, IP} = application:get_env(mdns_server_lib, ip),
     {ok, Port} = application:get_env(mdns_server_lib, port),
-    MDNSConfig=[{port, 5353},
-                {address, {224, 0, 0, 251}},
-                {domain, Domain},
-                {type, "_" ++ Service ++ "._tcp"},
-                {options,
-                 [{port, Port},
-                  {ip, IP}]}],
+    MDNSConfig0 = [{port, 5353},
+                   {address, {224, 0, 0, 251}},
+                   {domain, Domain},
+                   {type, "_" ++ Service ++ "._tcp"},
+                   {options,
+                    [{port, Port},
+                     {ip, IP}]}],
+    MDNSConfig =case  application:get_env(mdns_server_lib, interface) of
+                    {ok, IFace} ->
+                        [{interface, IFace} | MDNSConfig0];
+                    _ ->
+                        MDNSConfig0
+                end,
     [A, B, C, D] = [list_to_integer(binary_to_list(P)) || P <- re:split("10.0.0.10", "[.]")],
     {ok, _} = ranch:start_listener(mdns_server, 1,
                                    ranch_tcp, [{port, Port}, {ip, {A, B, C, D}}], mdns_server_protocol, []),
