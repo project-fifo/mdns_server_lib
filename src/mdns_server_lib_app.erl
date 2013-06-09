f3d431dde91cf8a7c1f832119fb19318480dee36
Heinz N. Gies <heinz@licenser.net>
2013-06-09 16:54:17 +0200

    Added disable switch.

--- src/mdns_server_lib_app.erl
+++ src/mdns_server_lib_app.erl
@@ -23,16 +23,21 @@ start(_StartType, _StartArgs) ->
                    {options,
                     [{port, Port},
                      {ip, IP}]}],
-    MDNSConfig =case  application:get_env(mdns_server_lib, interface) of
-                    {ok, IFace} ->
-                        [{interface, IFace} | MDNSConfig0];
-                    _ ->
-                        MDNSConfig0
-                end,
-    [A, B, C, D] = [list_to_integer(binary_to_list(P)) || P <- re:split(IP, "[.]")],
-    {ok, _} = ranch:start_listener(mdns_server, 1,
-                                   ranch_tcp, [{port, Port}, {ip, {A, B, C, D}}], mdns_server_protocol, []),
-    {ok, _} = mdns_server_supervisor:start_link([MDNSConfig]),
+    MDNSConfig = case application:get_env(mdns_server_lib, interface) of
+                     {ok, IFace} ->
+                         [{interface, IFace} | MDNSConfig0];
+                     _ ->
+                         MDNSConfig0
+                 end,
+    case application:get_env(mdns_server_lib, enabled) of
+        true ->
+            [A, B, C, D] = [list_to_integer(binary_to_list(P)) || P <- re:split(IP, "[.]")],
+            {ok, _} = ranch:start_listener(mdns_server, 1,
+                                           ranch_tcp, [{port, Port}, {ip, {A, B, C, D}}], mdns_server_protocol, []),
+            {ok, _} = mdns_server_supervisor:start_link([MDNSConfig]);
+        _ ->
+            ok
+    end,
     mdns_server_lib_sup:start_link().
 
 stop(_State) ->
