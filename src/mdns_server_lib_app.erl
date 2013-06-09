0dc31e63fefe9e1f7a1d756e219242f3637c2db3
Heinz N. Gies <heinz@licenser.net>
2013-06-09 16:54:52 +0200

    Switch is now propper disabled not enabled.

--- src/mdns_server_lib_app.erl
+++ src/mdns_server_lib_app.erl
@@ -29,14 +29,14 @@ start(_StartType, _StartArgs) ->
                      _ ->
                          MDNSConfig0
                  end,
-    case application:get_env(mdns_server_lib, enabled) of
-        true ->
+    case application:get_env(mdns_server_lib, disabled) of
+        {ok, true} ->
+            ok;
+        _ ->
             [A, B, C, D] = [list_to_integer(binary_to_list(P)) || P <- re:split(IP, "[.]")],
             {ok, _} = ranch:start_listener(mdns_server, 1,
                                            ranch_tcp, [{port, Port}, {ip, {A, B, C, D}}], mdns_server_protocol, []),
-            {ok, _} = mdns_server_supervisor:start_link([MDNSConfig]);
-        _ ->
-            ok
+            {ok, _} = mdns_server_supervisor:start_link([MDNSConfig])
     end,
     mdns_server_lib_sup:start_link().
 
